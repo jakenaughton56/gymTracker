7b730559cc95b6a9b316937526804e2887b4e2a6
Jake Naughton <jakenaughton56@gmail.com>
2018-11-19 20:13:26 +1000

    Initial Commit

---
+++ gymTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gymTracker
+//
+//  Created by Jake Mobile Dev on 19/11/18.
+//  Copyright © 2018 JakeMobileDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
